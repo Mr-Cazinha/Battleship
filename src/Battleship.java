fd7c633f9b2378c6a868652c44327e578fe78255
Shota0707 <computerscience@Locals-MacBook-Air.local>
2023-11-08 08:41:39 -1000

    ship

--- src/Battleship.java
+++ src/Battleship.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
 
 public class Battleship {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		public static void main(String[] args) {	
+			String start = getInput("Where fo you want to place your ");
+			
+			
+			
+		}
+		private static String[] ships() {
+		String[] ship = new String[] {"Carrier(5), Battleship(4), Cruiser(3), Submarine(3), Destroyer(2)"};
+		return ship;
 	}
-
-}
+			private static String getInput(String text) {
+				Scanner input;
+				String choice;
+				try 
+				{
+					input = new Scanner(System.in);
+					System.out.println(text);
+					choice = input.nextLine();
+				}catch(Exception e)
+				{
+					e.printStackTrace();
+					return null;
+				}
+				return choice;
+		}
+	}
